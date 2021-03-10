90c13a0aae9c1ecab3cf0e61b87fa543fd7b7667
Dongseon Lee <eastline@maycan.co.kr>
2021-03-10 10:31:29 +0900

    #1.1 Securing the Keys

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqxLAFUcDtn55bgjAfG_fZEshpY7tcpAA",
+  authDomain: "nwitter-b28ff.firebaseapp.com",
+  projectId: "nwitter-b28ff",
+  storageBucket: "nwitter-b28ff.appspot.com",
+  messagingSenderId: "783534155958",
+  appId: "1:783534155958:web:9dab3921c6e3b362c71e17",
+};
+
+export default firebase.initializeApp(firebaseConfig);
